b96313c58518bd4be817d32bf4909349c016309a
bigmoletos <bigmoletos@yopmail.com>
2019-09-22 12:16:14 +0200

    PrimitiveTraining.java, quete_Java training02:Primitive

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -1,18 +1,90 @@
 public class PrimitiveTraining {
 
-    // TODO 1 : Change the method 'integer' to return the expected result : int 42
-    public static int integer() {
-        return 0;
-    }
+	// TODO 1 : Change the method 'integer' to return the expected result : int 42
+	/**
+	 * @description
+	 *
+	 * @return int
+	 *
+	 * @method integer
+	 * @class PrimitiveTraining
+	 * @version 1.0
+	 * @date samedi 21 sept. 2019
+	 * @see
+	 *
+	 **/
+	public static int integer() {
+		return 42;
+	}
 
-    // TODO 2 : Create a method 'str', result expected : String "Hello"
+	// TODO 2 : Create a method 'str', result expected : String "Hello"
+	/**
+	 * @description
+	 *
+	 * @return String
+	 *
+	 * @method str
+	 * @class PrimitiveTraining
+	 * @version 1.0
+	 * @date samedi 21 sept. 2019
+	 * @see
+	 *
+	 **/
+	public static String str() {
+		return "Hello";
 
+	}
 
-    // TODO 3 : Create a method 'real', result expected : double 3.14
+	// TODO 3 : Create a method 'real', result expected : double 3.14
+	/**
+	 * @description
+	 *
+	 * @return double
+	 *
+	 * @method real
+	 * @class PrimitiveTraining
+	 * @version 1.0
+	 * @date samedi 21 sept. 2019
+	 * @see
+	 *
+	 **/
+	public static double real() {
+		return 3.14;
 
+	}
 
-    // TODO 4 : Create a method 'bool', result expected : boolean false
+	// TODO 4 : Create a method 'bool', result expected : boolean false
+	/**
+	 * @description
+	 *
+	 * @return boolean
+	 *
+	 * @method bool
+	 * @class PrimitiveTraining
+	 * @version 1.0
+	 * @date samedi 21 sept. 2019
+	 * @see
+	 *
+	 **/
+	public static boolean bool() {
+		return false;
 
+	}
 
-    // TODO 5 : Create a method 'nothing', no result expected
+	// TODO 5 : Create a method 'nothing', no result expected
+	/**
+	 * @description
+	 *
+	 * @return void
+	 *
+	 * @method nothing
+	 * @class PrimitiveTraining
+	 * @version 1.0
+	 * @date samedi 21 sept. 2019
+	 * @see
+	 *
+	 **/
+	public static void nothing() {
+
+	}
 }
