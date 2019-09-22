8ba238ebecf6ed073379f68ef7f353e6feb186bf
bigmoletos <bigmoletos@yopmail.com>
2019-09-22 16:22:27 +0200

    String training quete_Java training02: String

--- src/exercices/StringTraining.java
+++ src/exercices/StringTraining.java
@@ -1,169 +1,184 @@
 public class StringTraining {
 
-    /**
-     * @param firstname, ie : "Brandon"
-     * @return a string that concatenates "Hello " and firstname, ie : "Hello Brandon"
-     */
-    public static String helloFirstname(String firstname) {
+	/**
+	 * @param firstname, ie : "Brandon"
+	 * @return a string that concatenates "Hello " and firstname, ie : "Hello
+	 *         Brandon"
+	 */
+	public static String helloFirstname(String firstname) {
 
-        return "Hello ";
-    }
+		return "Hello " + firstname;
+	}
 
-    /**
-     * @param first  word, ie : "test"
-     * @param second word, ie : "value"
-     * @return a string with both word, ie : "testvalue"
-     */
-    public static String concatArgs(String first, String second) {
+	/**
+	 * @param first  word, ie : "test"
+	 * @param second word, ie : "value"
+	 * @return a string with both word, ie : "testvalue"
+	 */
+	public static String concatArgs(String first, String second) {
 
-        return "";
-    }
+		return first + second;
+	}
 
-    /**
-     * @param origin  string, eg: "test"
-     * @param compare string, eg: "sample"
-     * @return if origin string is equal to compare string
-     */
-    public static boolean equals(String origin, String compare) {
+	/**
+	 * @param origin  string, eg: "test"
+	 * @param compare string, eg: "sample"
+	 * @return if origin string is equal to compare string
+	 */
+	public static boolean equals(String origin, String compare) {
+		boolean res = false;
+		if (origin.equals(compare)) {
+			res = true;
+		}
+		return res;
+	}
 
-        return false;
-    }
+	/**
+	 * @param value character, eg: 't'
+	 * @return conversion of the character into String, eg: "t"
+	 */
+	public static String charToString(char value) {
+		String val = Character.toString(value);
+		return val;
+	}
 
-    /**
-     * @param value character, eg: 't'
-     * @return conversion of the character into String, eg: "t"
-     */
-    public static String charToString(char value) {
+	/**
+	 * @param value integer, eg: 3
+	 * @return convertion of the integer into String, eg: "3"
+	 */
+	public static String intToString(int value) {
+		String val = Integer.toString(value);
+		return val;
+	}
 
-        return "";
-    }
+	/**
+	 * @param string, eg: "test"
+	 * @return string length, eg: 4
+	 */
+	public static int length(String string) {
 
-    /**
-     * @param value integer, eg: 3
-     * @return convertion of the integer into String, eg: "3"
-     */
-    public static String intToString(int value) {
+		return string.length();
+	}
 
-        return "";
-    }
+	/**
+	 * @param string, ie "test"
+	 * @return string in upper case, eg: "TEST"
+	 */
+	public static String upper(String string) {
 
-    /**
-     * @param string, eg: "test"
-     * @return string length, eg: 4
-     */
-    public static int length(String string) {
+		return string.toUpperCase();
+	}
 
-        return 0;
-    }
+	/**
+	 * @param string, eg: "TEST"
+	 * @return string in lower case, eg: "test"
+	 */
+	public static String lower(String string) {
 
-    /**
-     * @param string, ie "test"
-     * @return string in upper case, eg: "TEST"
-     */
-    public static String upper(String string) {
+		return string.toLowerCase();
+	}
 
-        return "";
-    }
+	/**
+	 * @param string, eg: "test"
+	 * @return first character of the string, eg: 't'
+	 */
+	public static char firstChar(String string) {
 
-    /**
-     * @param string, eg: "TEST"
-     * @return string in lower case, eg: "test"
-     */
-    public static String lower(String string) {
+		return string.charAt(0);
+	}
 
-        return "";
-    }
+	/**
+	 * @param string, eg: "value"
+	 * @return last character of the string, eg: 'e'
+	 */
+	public static char lastChar(String string) {
 
-    /**
-     * @param string, eg: "test"
-     * @return first character of the string, eg: 't'
-     */
-    public static char firstChar(String string) {
+		return string.charAt(string.length() - 1);
+	}
 
-        return '*';
-    }
+	/**
+	 * @param string, eg: "test"
+	 * @param begin   position (inclusive) of the substring, eg: 1
+	 * @param end     postion (exclusive) of the substring, eg: 3
+	 * @return the substring from begin position to end position, eg: "es"
+	 */
+	public static String subString(String string, int begin, int end) {
+		// https://howtodoinjava.com/java/string/java-string-substring-example/
 
-    /**
-     * @param string, eg: "value"
-     * @return last character of the string, eg: 'e'
-     */
-    public static char lastChar(String string) {
+		return string.substring(begin, end);
+	}
 
-        return '*';
-    }
+	/**
+	 * @param string, eg: "test"
+	 * @return the string with the first character in upper case, eg: "Test"
+	 */
+	public static String capitalize(String string) {
 
-    /**
-     * @param string, eg: "test"
-     * @param begin   position (inclusive) of the substring, eg: 1
-     * @param end     postion (exclusive) of the substring, eg: 3
-     * @return the substring from begin position to end position, eg: "es"
-     */
-    public static String subString(String string, int begin, int end) {
-        // https://howtodoinjava.com/java/string/java-string-substring-example/
+		return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+	}
 
-        return "";
-    }
+	/**
+	 * @param string, eg: "test"
+	 * @param search  character, eg: 't'
+	 * @return the number of character occurrences in string, eg: 2
+	 */
+	public static int occurrences(String string, char search) {
+//		int count = StringUtils.countMatches("a.b.c.d", ".");
+//		int count = string.length() - string.replace(search, '').length();
+//		int count = string.replaceAll(Character.toString(search), "").length();
+//		long countjava8 = string.chars().filter(search -> search == '.').count();
+//		int countjava8 = new StringTokenizer(" " + string + " ", search).countTokens() - 1;
+		int count = 0;
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) == search) {
+				count++;
+			}
+		}
+		return count;
+	}
 
-    /**
-     * @param string, eg: "test"
-     * @return the string with the first character in upper case, eg: "Test"
-     */
-    public static String capitalize(String string) {
+	/**
+	 * @param string,  eg: "test"
+	 * @param search,  eg: 't'
+	 * @param replace, eg: 'w'
+	 * @return the string where searched characters are replaced, eg: "wesw"
+	 */
+	public static String replaceChar(String string, char search, char replace) {
+		String newString = string.replaceAll(Character.toString(search), Character.toString(replace));
+		return newString;
+	}
 
-        return "";
-    }
+	/**
+	 * @param string,  eg: "test"
+	 * @param search,  eg: "e"
+	 * @param replace, eg: "oa"
+	 * @return the string where searched substring are replaced, eg: "toast"
+	 */
+	public static String replaceString(String string, String search, String replace) {
+		String newString = string.replaceAll(search, replace);
+		return newString;
+	}
 
-    /**
-     * @param string, eg: "test"
-     * @param search  character, eg: 't'
-     * @return the number of character occurrences in string, eg: 2
-     */
-    public static int occurrences(String string, char search) {
+	/**
+	 * @param string,    eg: "test|value"
+	 * @param delimiter, eg: "|"
+	 * @return string array that contains values splited on the delimiter, eg:
+	 *         {"test", "value"}
+	 */
+	public static String[] split(String string, String delimiter) {
+		// https://howtodoinjava.com/java/string/java-string-split-example/
+		String[] newString = string.split(delimiter);
+		return newString;
+	}
 
-        return 0;
-    }
-
-    /**
-     * @param string,  eg: "test"
-     * @param search,  eg: 't'
-     * @param replace, eg: 'w'
-     * @return the string where searched characters are replaced, eg: "wesw"
-     */
-    public static String replaceChar(String string, char search, char replace) {
-
-        return "";
-    }
-
-    /**
-     * @param string,  eg: "test"
-     * @param search,  eg: "e"
-     * @param replace, eg: "oa"
-     * @return the string where searched substring are replaced, eg: "toast"
-     */
-    public static String replaceString(String string, String search, String replace) {
-
-        return "";
-    }
-
-    /**
-     * @param string,    eg: "test|value"
-     * @param delimiter, eg: "|"
-     * @return string array that contains values splited on the delimiter, eg: {"test", "value"}
-     */
-    public static String[] split(String string, String delimiter) {
-        // https://howtodoinjava.com/java/string/java-string-split-example/
-
-        return null;
-    }
-
-    /**
-     * @param strings    array, eg: {"test", "value"}
-     * @param delimiter, eg: ";"
-     * @return string of array values joined with the delimiter, ex: "test;value"
-     */
-    public static String join(String[] strings, String delimiter) {
-        // https://howtodoinjava.com/java8/java-8-join-string-array-example/
-
-        return "";
-    }
+	/**
+	 * @param strings    array, eg: {"test", "value"}
+	 * @param delimiter, eg: ";"
+	 * @return string of array values joined with the delimiter, ex: "test;value"
+	 */
+	public static String join(String[] strings, String delimiter) {
+		// https://howtodoinjava.com/java8/java-8-join-string-array-example/
+		String newString = String.join(delimiter, strings);
+		return newString;
+	}
 }
